9c653d9712f2bf956f984a225db514b4308fc9d2
hanhqvu <hanhqvu@users.noreply.github.com>
2022-06-01 21:37:17 +0900

    wip: styling

---
+++ script.js
@@ -0,0 +1,12 @@
+// const themeBtn = document.getElementById("theme-toggle");
+// const isDarkMode = document.body.classList.contains("dark-theme");
+// themeBtn.addEventListener("click", () => {
+//     if (isDarkMode) {
+//         document.body.classList.remove("dark-theme");
+//         document.body.classList.add("light-theme");
+//         }
+//     else {
+//         document.body.classList.add("dark-theme");
+//         document.body.classList.remove("light-theme");
+//     }
+// });
